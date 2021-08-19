d23e9b58a689cc920cdc98ce020644802543a3f3
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-08-19 14:11:48 -0600

    finished index.html and BasicJS to go with it

---
+++ Basic Javascript Projects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+// first variable:
+var D = "Dapper D3mon";
+// second variable:
+var X = " is my nickname";
+var str = "Red Text Here";
+var Y = 7 * 3;
+// firts doc write:
+document.write(str.fontcolor("red"));
+// window allert here:
+window.alert("Dapper D3mon!")
+document.write("Some say, \"There's a hevan for those who will wait!\" \"I'd rather laugh with the sinners, than cry with the saints!!\" Billy responded.");
+document.getElementById("total").innerHTML = "Some say, \" There is a hevan for those who will wait! \" ";
+document.getElementById("total2").innerHTML = " \"I'd rather laugh with the sinners, than cry with the saints!!\" Billy responded.";
+// first expression:
+document.getElementById("total3").innerHTML = D + X;
