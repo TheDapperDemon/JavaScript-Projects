a30026ae9246e423a52de25697b40026ecfb3ef0
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-08-19 15:40:04 -0600

    project2 function

---
+++ Basic Javascript Projects/project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+var G = 19;
+var A = 34;
+document.getElementById("total").innerHTML = G * A;
+function myFunction() {
+    var sentence = "I am Learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concentrate").innerHTML = sentence;
+}
