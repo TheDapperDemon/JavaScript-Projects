1a9d5d4b9e6f947aee143285c1fbf622ff692d57
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-08-19 15:47:21 -0600

    fixed button on project2 function

--- Basic Javascript Projects/project2_functions/JS/main.js
+++ Basic Javascript Projects/project2_functions/JS/main.js
@@ -1,6 +1,9 @@
+// these two variables will be used to execute line 5:
 var G = 19;
 var A = 34;
+// this is an expression of assigning the varriables above together through multiplication:
 document.getElementById("total").innerHTML = G * A;
+// This is a function utulizing the += elemant as well as the document.getElementById:
 function myFunction() {
     var sentence = "I am Learning";
     sentence += " a lot from this book!";
