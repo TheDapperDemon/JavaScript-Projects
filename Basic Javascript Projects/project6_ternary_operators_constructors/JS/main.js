46abd752b066c7012dc128eab57abe443e588f54
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-08-30 13:57:10 -0600

    created project 6

---
+++ Basic Javascript Projects/project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,31 @@
+
+function Ride_Function(){
+    var Height, Can_ride;
+    Height = document.getElementById("Height").nodeValue;
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough!";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+class Customer
+    {
+        string fullName;
+        Boolean Active;
+        Customer(string NamedNodeMap){
+            Fullname = NamedNodeMap;
+            Active = true;
+        }
+    }
+
+    // Customer list:
+
+                Customer cust = new Customer("Jonah Diaz");
+
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
