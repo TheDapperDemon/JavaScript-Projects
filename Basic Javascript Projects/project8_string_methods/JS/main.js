6fca3e23b7bfb851e52c4f1be38eb1e2874ccf91
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-09-01 15:47:20 -0600

    fixed slice and concat

--- Basic Javascript Projects/project8_string_methods/JS/main.js
+++ Basic Javascript Projects/project8_string_methods/JS/main.js
@@ -11,7 +11,7 @@ function full_Sentence() {
 // Slice method function :
 function slice_Method() {
     var Sentence = "All work and no play makes Johnny a dull boy...";
-    var Section = Sentence.slice(27,33);
+    var Section = Sentence.slice(0,47);
     document.getElementById("Slice").innerHTML = Section;
 }
 
