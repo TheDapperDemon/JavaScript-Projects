876a4368e8ee8cfad3ef4776776eeabd04fb558f
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-09-07 14:35:35 -0600

    created project 10

---
+++ Basic Javascript Projects/project10_loops_arrays/JS/main.js
@@ -0,0 +1,35 @@
+// While loop function:
+
+function count_To_Ten() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting_To_Ten").innerHTML = Digit;
+}
+
+// For loop function:
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+// Array function 
+
+function cat_pics() {
+    var Cat_Pictures = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "eating";
+    Cat_Picture[2] = "playing";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this pic, the cat is " + 
+        Cat_Picture[2] + ".";
+}
