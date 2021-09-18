057dfb23b6f6b2fd1516c449fa4c09682bf6777c
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-09-18 15:30:50 -0600

    updated JS

--- TicTacToe/JS/tictactoe.js
+++ TicTacToe/JS/tictactoe.js
@@ -46,5 +46,35 @@ function placeXOrO(squareNumber) {
 
 function checkWinConditions() {
     if         (arrayIncludes('OX', '1X', '2X')) { drawWinLine(50, 100, 588, 100) }
-    else if    (arrayIncludes('OX', '1X', '2X')) { drawWinLine(0, 0, 0, 0) }
+    else if    (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
+    else if    (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 304) }
+    else if    (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558) }
+    else if    (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 100, 558) }
+    else if    (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558) }
+    else if    (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90) }
+    else if    (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520) }
+    else if    (arrayIncludes('00', '10', '20')) { drawWinLine(50, 100, 558, 100) }
+    else if    (arrayIncludes('30', '40', '50')) { drawWinLine(50, 304, 558, 304) }
+    else if    (arrayIncludes('60', '70', '80')) { drawWinLine(50, 508, 558, 508) }
+    else if    (arrayIncludes('00', '30', '60')) { drawWinLine(100, 50, 100, 558) }
+    else if    (arrayIncludes('10', '40', '70')) { drawWinLine(304, 50, 304, 558) }
+    else if    (arrayIncludes('20', '50', '80')) { drawWinLine(508, 50, 508, 558) }
+    else if    (arrayIncludes('60', '40', '20')) { drawWinLine(100, 508, 510, 90) }
+    else if    (arrayIncludes('00', '40', '80')) { drawWinLine(100, 100, 520, 520) }
+    else if    (selectedSquares.length >= 9) {
+        audio("Media/tie.mp3");
+        setTimeouit(function () { resetGame(); }, 1000);
+    }
+    function arrayIncludes(squareA, squareB, SquareC) {
+        const a = selectedSquares.includes(squareA)
+        const b = selectedSquares.includes(squareB)
+        const c = selectedSquares.includes(squareC)
+        if (a === true && b === true && c === true) { return true }
+    }
+}
+
+
+function disableClick() {
+    body.style.pointerEvents = "none";
+    setTimeout(function()  {body.style.pointerEvents = 'auto';}, 1000);
 }
