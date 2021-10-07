6d02595c6e0df4a4f1f88134f89273bf7bc0266a
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2021-10-07 09:01:00 -0600

    updated css and js

---
+++ todo_app/JS/todo.js
@@ -0,0 +1,33 @@
+function get_Todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str)
+    }
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    document.getElementById("task").value = '';
+    show();
+
+    return false;
+}
+
+function show() {
+    var todos = get_todos();
+    var html = '<ul>';
+    for (var i = 0; i < todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></lt>';
+    };
+    html += '<ul>';
+    document.getElementById('todos').innerHTML = html;
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
